042411d41db75a1b600c2feacc2151519d97d0c0
Kevin Rowe <kev.rowe@gmail.com>
2024-04-08 21:03:38 +0800

    Add tests for commands

---
+++ src/commands/addCar.test.ts
@@ -0,0 +1,57 @@
+import { IO } from '../io'
+import { cliAddCar } from './addCar'
+
+describe('cliAddCar', () => {
+  it('should add a car to the simulation with invalid followed by valid inputs', async () => {
+    const inputs = ['A', '1 1 N', 'ASDA', 'FRFF']
+
+    const mockRead = jest.fn()
+    inputs.forEach((e) => mockRead.mockResolvedValueOnce(e))
+
+    const io: IO = {
+      read: mockRead,
+      write: jest.fn(),
+    }
+
+    const simulation = {
+      fieldWidth: 10,
+      fieldHeight: 10,
+    }
+
+    const car = await cliAddCar(io, simulation)
+
+    expect(car).toEqual({
+      name: 'A',
+      heading: 'N',
+      commands: 'FRFF',
+      x: 1,
+      y: 1,
+    })
+  })
+  it('should add a car to the simulation with valid inputs', async () => {
+    const inputs = ['A', '1 1 N', 'FRFF']
+
+    const mockRead = jest.fn()
+    inputs.forEach((e) => mockRead.mockResolvedValueOnce(e))
+
+    const io: IO = {
+      read: mockRead,
+      write: jest.fn(),
+    }
+
+    const simulation = {
+      fieldWidth: 10,
+      fieldHeight: 10,
+    }
+
+    const car = await cliAddCar(io, simulation)
+
+    expect(car).toEqual({
+      name: 'A',
+      heading: 'N',
+      commands: 'FRFF',
+      x: 1,
+      y: 1,
+    })
+  })
+})
---
+++ src/commands/mainMenu.test.ts
@@ -0,0 +1,42 @@
+import { mainMenu } from './mainMenu'
+import { IO } from '../io'
+import { Car } from '../types/Car'
+import { Simulation } from '../types/Simulation'
+import * as AddCar from './addCar'
+
+describe('mainMenu', () => {
+  it('should return an array of cars when 2 are added', async () => {
+    const mockRead = jest.fn()
+    const io: IO = {
+      read: mockRead,
+      write: jest.fn(),
+    }
+
+    const simulation: Simulation = {
+      fieldHeight: 3,
+      fieldWidth: 3,
+    }
+
+    const mockCar: Car = {
+      commands: 'FRLFF',
+      heading: 'N',
+      name: 'A',
+      x: 2,
+      y: 1,
+    }
+
+    const options = ['1', '1', '2']
+    options.forEach(mockRead.mockResolvedValueOnce)
+
+    const mockCLIAddCar = jest
+      .spyOn(AddCar, 'cliAddCar')
+      .mockResolvedValue(mockCar)
+
+    const result: Car[] = await mainMenu(io, simulation)
+
+    expect(result).toBeInstanceOf(Array)
+    expect(result.length).toEqual(2)
+    expect(mockCLIAddCar).toHaveBeenCalledTimes(2)
+    expect(result[0]).toEqual(mockCar)
+  })
+})
